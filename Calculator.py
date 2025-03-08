0aea6338204175e439c0a72e36a916411d48d626
Dennsm <dennis.smg@hotmail.com>
2025-03-08 05:42:42 -0600

    second commit

---
+++ Calculator.py
@@ -0,0 +1,49 @@
+#Proyecto Calculadora
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        print("No se puede dividir por 0")
+        return
+    
+    return a/b
+#Salir
+def salir():
+    print("Salio")
+
+print("Calculador 1.0")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Salir")
+opt = input("Introduzca una opción válida: ")
+
+if int(opt) >= 1 and int(opt) <= 5:
+    
+    if opt == "5":
+        salir()
+    else:
+        a = float(input ("Introduzca el número a: "))
+        b = float(input ("Introduzca el número b: "))
+    
+        if opt == "1":
+            print("La suma es: ", sumar(a, b))
+            
+        elif opt == "2":
+            print("La resta es: ", restar(a, b))
+        elif opt == "3":
+            print("La multiplicación es: ", multiplicar(a, b))
+        elif opt =="4":
+            print("La división es: ", dividir(a, b))
+        
+else:
+    print("No es una opcion válida! ")
---
+++ README.md
@@ -0,0 +1,1 @@
+#Hola mundo Prueba Github
